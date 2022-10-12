1aca01bd41ad4ed86747225972c17735aa8c16f7
gyanu <gyanu1614@gmail.com>
2022-10-12 21:58:32 +0530

    First Commit

---
+++ backend/base/products.py
@@ -0,0 +1,83 @@
+products = [
+    {
+        '_id': '1',
+        'name': 'Dueling Dragons | Goal Explosion',
+        'image': '/images/DD.png',
+        'description':
+        'Black Market Goal Explosion',
+        'brand': 'Apple',
+        'category': 'Electronics',
+        'price': 7.95,
+        'countInStock': 0,
+        'rating': 4.5,
+        'numReviews': 12,
+    },
+    {
+        '_id': '2',
+        'name': 'Black Dieci | Exotic',
+        'image': '/images/Dieci.png',
+        'description':
+        'Black Dieci Exotic Wheels',
+        'brand': 'Apple',
+        'category': 'Electronics',
+        'price': 204.95,
+        'countInStock': 7,
+        'rating': 4.0,
+        'numReviews': 8,
+
+    },
+    {
+        '_id': '3',
+        'name': 'Mainframe | Black Market Decal',
+        'image': '/images/Mainframe.png',
+        'description': 'Black Market Decal',
+        'brand': 'Cannon',
+        'category': 'Electronics',
+        'price': 7.95,
+        'countInStock': 5,
+        'rating': 3,
+        'numReviews': 12,
+
+    },
+    {
+        '_id': '4',
+        'name': 'Zomba | Titanium White',
+        'image': '/images/Zomba.png',
+        'description':
+        'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+        'brand': 'Sony',
+        'category': 'Electronics',
+        'price': 15.99,
+        'countInStock': 11,
+        'rating': 5,
+        'numReviews': 12,
+
+    },
+    {
+        '_id': '5',
+        'name': 'Alpha Cap | Gold Cap',
+        'image': '/images/AlphaCap.jpg',
+        'description':
+        'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+        'brand': 'Logitech',
+        'category': 'Electronics',
+        'price': 549.89,
+        'countInStock': 7,
+        'rating': 3.5,
+        'numReviews': 10,
+
+    },
+    {
+        '_id': '6',
+        'name': 'Octane | Titanium White',
+        'image': '/images/Octane.png',
+        'description':
+        'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+        'brand': 'Amazon',
+        'category': 'Electronics',
+        'price': 59.95,
+        'countInStock': 0,
+        'rating': 4,
+        'numReviews': 12,
+    },
+]
